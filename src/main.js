e7e1a127558e7273fb5a3ad0df0f661a7f890e20
Imran <developerimran.in@gmail.com>
2024-08-26 16:10:45 +0600

    main js file update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 const student_create_form = document.getElementById("student-create");
+const msg = document.querySelector(".msg");
 
 student_create_form.onsubmit = (e) => {
   e.preventDefault();
 
   const form_data = new FormData(e.target);
-  const data = Object.fromEntries(form_data);
-  console.log(data);
+  const { name, email, phone, location, photo } = Object.fromEntries(form_data);
+  if (!name || !email || !phone || !location || !photo) {
+    msg.innerHTML = createAlert("All fields are requard");
+  } else if (!isEmail(email)) {
+    msg.innerHTML = createAlert("Invalid email Address", "warning");
+  } else if (!isMobile(phone)) {
+    msg.innerHTML = createAlert("Invalid Mobile Nubmer", "warning");
+  }
 };
